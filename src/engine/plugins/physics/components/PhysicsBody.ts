4706adae940e17191e33c94a3861a0c62270080a
Andrew Munro <synestry@gmail.com>
2020-04-10 13:39:13 +0100

    🎨  Adding getters to physicsbody

--- src/engine/plugins/physics/components/PhysicsBody.ts
+++ src/engine/plugins/physics/components/PhysicsBody.ts
@@ -50,47 +50,87 @@ export default class PhysicsBody {
 		Body.translate(this.body, translation);
 	}
 
+	public center(center: { x: number; y: number }, relative = false) {
+		Body.setCentre(this.body, center, relative);
+	}
+
 	public set position(position: { x: number; y: number }) {
 		Body.setPosition(this.body, position);
+	}
+
+	public get position() {
+		return this.body.position;
 	}
 
 	public set velocity(velocity: { x: number; y: number }) {
 		Body.setVelocity(this.body, velocity);
 	}
 
+	public get velocity() {
+		return this.body.velocity;
+	}
+
 	public set mass(mass: number) {
 		Body.setMass(this.body, mass);
+	}
+
+	public get mass() {
+		return this.body.mass;
 	}
 
 	public set density(density: number) {
 		Body.setDensity(this.body, density);
 	}
 
+	public get density() {
+		return this.body.density;
+	}
+
 	public set inertia(inertia: number) {
 		Body.setInertia(this.body, inertia);
+	}
+
+	public get inertia() {
+		return this.body.inertia;
 	}
 
 	public set vertices(vertices: Array<{ x: number; y: number }>) {
 		Body.setVertices(this.body, vertices);
 	}
 
+	public get vertices() {
+		return this.body.vertices;
+	}
+
 	public set parts(parts: Body[]) {
 		Body.setParts(this.body, parts);
 	}
 
-	public set center(center: { x: number; y: number }) {
-		Body.setCentre(this.body, center);
+	public get parts() {
+		return this.body.parts;
 	}
 
 	public set angle(angle: number) {
 		Body.setAngle(this.body, angle);
 	}
 
+	public get angle() {
+		return this.body.angle;
+	}
+
 	public set angularVelocity(velocity: number) {
 		Body.setAngularVelocity(this.body, velocity);
 	}
 
+	public get angularVelocity() {
+		return this.body.angularVelocity;
+	}
+
 	public set static(isStatic: boolean) {
 		Body.setStatic(this.body, isStatic);
+	}
+
+	public get static() {
+		return this.body.isStatic;
 	}
 }
