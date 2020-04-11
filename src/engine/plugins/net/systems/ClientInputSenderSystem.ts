59b6573ffb24efe78e2b72b9d9b66d4bae0f8093
Lucas Jones <lucas.jones@skybettingandgaming.com>
2020-04-11 10:07:21 +0100

    📝  ClientInputSenderSystem now uses `updateEntityFixed`

--- src/engine/plugins/net/systems/ClientInputSenderSystem.ts
+++ src/engine/plugins/net/systems/ClientInputSenderSystem.ts
@@ -3,24 +3,21 @@ import Input from '@ecs/plugins/input/components/Input';
 import { IterativeSystem } from '@ecs/ecs/IterativeSystem';
 import Session from '../components/Session';
 import { PacketOpcode } from '../components/Packet';
+import { Entity } from '@ecs/ecs/Entity';
 
 export default class ClientInputSenderSystem extends IterativeSystem {
 	constructor() {
 		super(makeQuery(all(Session, Input)));
 	}
 
-	public update(deltaTime: number) {
-		super.update(deltaTime);
+	protected updateEntityFixed(entity: Entity, deltaTime: number) {
+		const session = entity.get(Session);
+		const input = entity.get(Input);
 
-		this.query.entities.forEach(entity => {
-			const session = entity.get(Session);
-			const input = entity.get(Input);
-
-			session.socket.send({
-				opcode: PacketOpcode.PLAYER_INPUT,
-				input: input,
-				tick: session.serverTick
-			});
+		session.socket.send({
+			opcode: PacketOpcode.PLAYER_INPUT,
+			input: input,
+			tick: session.serverTick
 		});
 	}
 }
