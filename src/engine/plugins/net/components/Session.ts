9b9cba0665a71b3b98fe3cd737bbf18ec5fa977d
Lucas Jones <lucas.jones@skybettingandgaming.com>
2020-04-11 13:41:09 +0100

    ✨  Disconnects clients if fail to respond to PING

--- src/engine/plugins/net/components/Session.ts
+++ src/engine/plugins/net/components/Session.ts
@@ -1,5 +1,11 @@
 import Socket from '../utils/Socket';
 
 export default class Session {
-	constructor(public id: string, public socket: Socket, public serverTime = 0, public serverTick = 0) {}
+	constructor(
+		public id: string,
+		public socket: Socket,
+		public serverTime = 0,
+		public serverTick = 0,
+		public lastPongResponse: number = -1
+	) {}
 }
