2c305e5c3fa69358c6654677c23342db584b3d59
Lucas Jones <lucas.jones@skybettingandgaming.com>
2020-04-14 17:52:27 +0100

    🔥  Started input / sync work

--- src/playground/systems/MovementSystem.ts
+++ src/playground/systems/MovementSystem.ts
@@ -1,37 +1,20 @@
-import { IterativeSystem } from '@ecs/ecs/IterativeSystem';
 import { Entity } from '@ecs/ecs/Entity';
-import Input, { InputHistory } from '@ecs/plugins/input/components/Input';
-import Moveable from '../components/Moveable';
-import { makeQuery, all, any } from '@ecs/utils/QueryHelper';
-import PhysicsBody from '@ecs/plugins/physics/components/PhysicsBody';
+import { IterativeSystem } from '@ecs/ecs/IterativeSystem';
+import Input from '@ecs/plugins/input/components/Input';
 import Session from '@ecs/plugins/net/components/Session';
+import PhysicsBody from '@ecs/plugins/physics/components/PhysicsBody';
+import { all, makeQuery } from '@ecs/utils/QueryHelper';
+import Moveable from '../components/Moveable';
 
 export default class MovementSystem extends IterativeSystem {
 	constructor() {
-		super(makeQuery(all(Session, Moveable, PhysicsBody), any(Input, InputHistory)));
+		super(makeQuery(all(Session, Moveable, PhysicsBody, Input)));
 	}
 
 	protected updateEntityFixed(entity: Entity, dt: number) {
-		const session = entity.get(Session);
 		const body = entity.get(PhysicsBody);
 		const moveable = entity.get(Moveable);
-
-		let input = entity.get(Input);
-		// must be server
-		if (!input) {
-			const history = entity.get(InputHistory);
-
-			if (history.inputs[session.serverTick]) {
-				// set input from player history
-				input = history.inputs[session.serverTick];
-			} else {
-				// Set the last sent input & reuse that
-				const keys = Object.keys(history.inputs);
-				const frame = keys.pop();
-				input = history.inputs[frame];
-				// console.warn(`⚠️  Missing player input - using last input ${frame}`);
-			}
-		}
+		const input = entity.get(Input);
 
 		if (input) {
 			const left = input.leftDown ? 1 : 0;
