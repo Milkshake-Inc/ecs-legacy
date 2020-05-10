03022d4434363fd0c45dbee7e3b789a414d06363
Lucas Jones <lucas.jones@skybettingandgaming.com>
2020-05-10 20:43:53 +0100

    🔌  Basic networking

--- src/playground/examples/spaces/network/NetworkClientSpace.ts
+++ src/playground/examples/spaces/network/NetworkClientSpace.ts
@@ -1,24 +1,40 @@
-import BaseSpace from '../../BaseSpace';
-import BoatEntity from '@ecs/plugins/vehicle/entity/BoatEntity';
+import { useQueries } from '@ecs/ecs/helpers';
 import Vector3 from '@ecs/math/Vector';
-import { functionalSystem } from '@ecs/ecs/helpers';
-import { all } from '@ecs/utils/QueryHelper';
+import ThirdPersonCameraSystem from '@ecs/plugins/3d/systems/ThirdPersonCameraSystem';
+import ThirdPersonTarget from '@ecs/plugins/3d/systems/ThirdPersonTarget';
+import { ClientBasicWorldSnapshotSystem } from '@ecs/plugins/net/systems/ClientBasicWorldSnapshotSystem';
+import ClientConnectionSystem from '@ecs/plugins/net/systems/ClientConnectionSystem';
+import ClientPingSystem from '@ecs/plugins/net/systems/ClientPingSystem';
+import CannonBody from '@ecs/plugins/physics/components/CannonBody';
 import Boat from '@ecs/plugins/vehicle/components/Boat';
-import { Transform } from 'pixi.js';
+import BoatEntity from '@ecs/plugins/vehicle/entity/BoatEntity';
+import { all } from '@ecs/utils/QueryHelper';
+import BaseSpace from '../../BaseSpace';
+import { deserialize, Snapshot } from './Snapshot';
+
+export class BoatClientWorldSnapshotSystem extends ClientBasicWorldSnapshotSystem<Snapshot> {
+	protected snapshotQueries = useQueries(this, {
+		boats: all(Boat)
+	});
+
+	createEntitiesFromSnapshot(snapshot) {}
+
+	applySnapshot(snapshot: Snapshot) {
+		const boat = this.snapshotQueries.boats.first;
+		const body = boat.get(CannonBody);
+		deserialize(body, snapshot.boat);
+	}
+}
 
 export class NetworkClientSpace extends BaseSpace {
 	setup() {
+		this.addSystem(new ClientConnectionSystem(this.worldEngine), 1000); // has to be low priority so systems get packets before the queue is cleared
+		this.addSystem(new ClientPingSystem());
+		this.addSystem(new BoatClientWorldSnapshotSystem());
+		this.addSystem(new ThirdPersonCameraSystem());
 		super.setup();
-
 		const boat = new BoatEntity(this.boatModel, new Vector3(0, 10, 0));
+		boat.add(ThirdPersonTarget);
 		this.addEntity(boat);
-
-		this.addSystem(
-			functionalSystem([all(Boat)], {
-				entityUpdateFixed: (entity, dt) => {
-					console.log(entity.get(Transform).position.y);
-				}
-			})
-		);
 	}
 }
--- src/playground/examples/spaces/network/NetworkServerSpace.ts
+++ src/playground/examples/spaces/network/NetworkServerSpace.ts
@@ -1,23 +1,49 @@
 // import Space from '@ecs/plugins/space/Space';
-import BaseSpace from '../../BaseSpace';
+import { functionalSystem, useQueries } from '@ecs/ecs/helpers';
 import Vector3 from '@ecs/math/Vector';
-import BoatEntity from '@ecs/plugins/vehicle/entity/BoatEntity';
+import { ServerWorldSnapshotSystem } from '@ecs/plugins/net/systems/ServerWorldSnapshotSystem';
+import CannonBody from '@ecs/plugins/physics/components/CannonBody';
 import Boat from '@ecs/plugins/vehicle/components/Boat';
+import BoatEntity from '@ecs/plugins/vehicle/entity/BoatEntity';
 import { all } from '@ecs/utils/QueryHelper';
-import { functionalSystem } from '@ecs/ecs/helpers';
-import Transform from '@ecs/plugins/Transform';
+import { Vec3 } from 'cannon-es';
+import BaseSpace from '../../BaseSpace';
+import { serialize, Snapshot } from './Snapshot';
+
+export class BoatServerWorldSnapshotSystem extends ServerWorldSnapshotSystem<Snapshot> {
+	protected snapshotQueries = useQueries(this, {
+		boats: all(Boat),
+	})
+
+	generateSnapshot(): Snapshot {
+		const boat = this.snapshotQueries.boats.first;
+		const body = boat.get(CannonBody);
+
+		return {
+			boat: serialize(body),
+		}
+	}
+}
 
 export class NetworkServerSpace extends BaseSpace {
 	setup() {
 		super.setup();
 
+
+		this.addSystem(new BoatServerWorldSnapshotSystem());
+
+
 		const boat = new BoatEntity(this.boatModel, new Vector3(0, 10, 0));
 		this.addEntity(boat);
+
+		setInterval(() => {
+			boat.get(CannonBody).applyImpulse(new Vec3(4, 80, 0), new Vec3(0, 0, 0));
+		}, 1000);
 
 		this.addSystem(
 			functionalSystem([all(Boat)], {
 				entityUpdateFixed: (entity, dt) => {
-					console.log(entity.get(Transform).position.y);
+					// console.log(entity.get(Transform).position.y);
 				}
 			})
 		);
