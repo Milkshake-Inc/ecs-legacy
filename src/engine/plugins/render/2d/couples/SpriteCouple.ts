4bba977b6531faff2c2b24f1a66fa35627a05cc0
Lucas Jones <lucas.jones@skybettingandgaming.com>
2020-09-16 23:26:37 +0100

    Added removed sprite couple code

--- src/engine/plugins/render/2d/couples/SpriteCouple.ts
+++ src/engine/plugins/render/2d/couples/SpriteCouple.ts
@@ -12,17 +12,17 @@ export const useSpriteCouple = (system: System) =>
 			return PixiSprite.from(imageUrl);
 		},
 		onUpdate: (entity, displayObject) => {
-			// const sprite = entity.get(Sprite);
+			const sprite = entity.get(Sprite);
 
-			// if (sprite.frame && displayObject.texture.baseTexture.resource.valid) {
-			// 	displayObject.texture.frame = sprite.frame;
-			// }
+			if (sprite.frame && displayObject.texture.baseTexture.resource.valid) {
+				displayObject.texture.frame = sprite.frame;
+			}
 
-			// if (sprite.imageUrl && (displayObject.texture.baseTexture.resource as resources.ImageResource).url) {
-			// 	displayObject.texture = Texture.from(sprite.imageUrl);
-			// }
+			if (sprite.imageUrl && (displayObject.texture.baseTexture.resource as resources.ImageResource).url) {
+				displayObject.texture = Texture.from(sprite.imageUrl);
+			}
 
-			// displayObject.tint = sprite.tint;
-			// displayObject.anchor.set(sprite.anchor.x, sprite.anchor.y);
+			displayObject.tint = sprite.tint;
+			displayObject.anchor.set(sprite.anchor.x, sprite.anchor.y);
 		}
 	});
