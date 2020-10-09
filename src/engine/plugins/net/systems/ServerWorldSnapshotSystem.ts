3722b51fef8f09cd9d3bb0433af59c80bc66cc5d
Andrew Munro <andrew.munro@skybettingandgaming.com>
2020-10-09 17:53:11 +0100

    Don't send snapshots to everyone

--- src/engine/plugins/net/systems/ServerWorldSnapshotSystem.ts
+++ src/engine/plugins/net/systems/ServerWorldSnapshotSystem.ts
@@ -1,10 +1,17 @@
+import { useQueries } from '@ecs/core/helpers';
+import { all } from '@ecs/core/Query';
 import { System } from '@ecs/core/System';
 import { PacketOpcode } from '@ecs/plugins/net/components/Packet';
+import Session from '../components/Session';
 import { useBaseNetworking } from '../helpers/useNetworking';
 
 export abstract class ServerWorldSnapshotSystem<S extends {}> extends System {
 	protected elaspedMs: number;
 	protected updateMs: number;
+
+	protected queries = useQueries(this, {
+		sessions: all(Session)
+	});
 
 	protected networking = useBaseNetworking(this);
 
@@ -21,10 +28,14 @@ export abstract class ServerWorldSnapshotSystem<S extends {}> extends System {
 		if (this.elaspedMs >= this.updateMs) {
 			this.elaspedMs -= this.updateMs;
 
-			this.networking.send({
+			const packet = {
 				opcode: PacketOpcode.WORLD,
 				tick: 0,
 				snapshot: this.generateSnapshot()
+			};
+
+			this.queries.sessions.forEach(entity => {
+				this.networking.sendTo(entity, packet as any);
 			});
 		}
 	}
