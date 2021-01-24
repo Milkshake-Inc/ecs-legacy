ff4c50727da74c48dc4216d2db091da85c8b1750
Lucas Jones <Necxii@gmail.com>
2021-01-24 14:57:51 +0000

    Added GradientSkybox & TextureSkybox

---
+++ src/engine/plugins/render/3d/prefabs/TextureSkybox.ts
@@ -0,0 +1,29 @@
+import Transform from '@ecs/plugins/math/Transform';
+import { LoadTexture } from '@ecs/plugins/tools/ThreeHelper';
+import { BoxGeometry, DoubleSide, Mesh, MeshBasicMaterial, RepeatWrapping, sRGBEncoding } from 'three';
+import { Entity } from 'tick-knock';
+
+export const generateTextureSkybox = async (textureUrls: [right?: string, left?: string, up?: string, down?: string, back?: string, front?: string,], size = 2000) => {
+	const textures = await Promise.all(textureUrls.map(LoadTexture));
+
+	textures.forEach(element => {
+		element.wrapS = RepeatWrapping;
+		element.repeat.set(-1, 1);
+		element.encoding = sRGBEncoding;
+	});
+
+	const sky = new Entity();
+	sky.add(Transform);
+	sky.add(
+		new Mesh(
+			new BoxGeometry(size, size, size),
+			textures.map(texture => new MeshBasicMaterial({
+				map: texture,
+				side: DoubleSide,
+				fog: false
+			}))
+		)
+	)
+
+	return sky;
+};
